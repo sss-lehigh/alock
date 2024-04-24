bda138a132772a19ccf4e59827c07bdf89a5e3a0
amandabaran <amandabaran9@gmail.com>
2024-04-24 11:52:28 -0400

    added gitignore

--- build/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/amanda/sss/alock
-# Build directory: /home/amanda/sss/alock/build
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
